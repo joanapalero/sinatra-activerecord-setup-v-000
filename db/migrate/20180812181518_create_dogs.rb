55c33c3d3444acbc02909c4d640509e020477ae2
Joana Palero <joana.palero24@gmail.com>
2018-08-12 18:18:49 +0000

    automatically backed up by learn

--- db/migrate/20180812181518_create_dogs.rb
+++ db/migrate/20180812181518_create_dogs.rb
@@ -5,4 +5,14 @@ class CreateDogs < ActiveRecord::Migration
       t.string :breed
     end
   end
+  def up
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
+
+  def down
+    drop_table :dogs
+  end
 end
